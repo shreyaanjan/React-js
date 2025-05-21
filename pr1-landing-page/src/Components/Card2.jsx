d11c26ab311795001e2dc352a1c619de892f007a
shreyaanjan <shreyaanjan02@gmail.com>
2025-05-21 17:10:39 +0530

    added some hover effects

--- pr1-landing-page/src/Components/Card2.jsx
+++ pr1-landing-page/src/Components/Card2.jsx
@@ -3,17 +3,15 @@ import React from 'react'
 const Card2 = (props) => {
   return (
     <div className="col-lg-3 col-md-6 col-12">
-      <div className="">
-        <div className="card-body2 text-center">
-          <img src={props.img} className="rounded-circle mt-3" />
-          <h5 className="card-title2 my-4 fw-semibold">{props.name}</h5>
-          <p className="card-text2 text-secondary">{props.desc}</p>
-          <div className="d-flex align-items-center justify-content-center gap-3 stars">
-            <i class="bi bi-star fs-4"></i>
-            <i class="bi bi-star fs-4"></i>
-            <i class="bi bi-star fs-4"></i>
-            <i class="bi bi-star fs-4"></i>
-          </div>
+      <div className="card-body2 text-center">
+        <img src={props.img} className="rounded-circle mt-3" />
+        <h5 className="card-title2 my-4 fw-semibold">{props.name}</h5>
+        <p className="card-text2">{props.desc}</p>
+        <div className="d-flex align-items-center justify-content-center gap-3 stars">
+          <i class="bi bi-star fs-4"></i>
+          <i class="bi bi-star fs-4"></i>
+          <i class="bi bi-star fs-4"></i>
+          <i class="bi bi-star fs-4"></i>
         </div>
       </div>
     </div>
--- pr1-landing-page/src/Components/PortCard.jsx
+++ pr1-landing-page/src/Components/PortCard.jsx
@@ -2,7 +2,7 @@ const PortCard = (props) => {
     return (
         <div className="col-lg-4 col-md-6 col-12">
             <div className="h-100">
-                <div className="portCard ">
+                <div className="portCard">
                     <div className={`card-body3  ${props.effect} text-center`}>
                         <h4 className="card-title text-white fw-semibold">{props.title}</h4>
                         <p className="card-text my-4">{props.desc}</p>
