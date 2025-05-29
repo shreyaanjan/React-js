0b264bf4b029bbe842113accd1a3d0e0995af59f
shreyaanjan <shreyaanjan02@gmail.com>
2025-05-29 13:42:28 +0530

    design done

--- pr3-todo-list/src/components/Table.jsx
+++ pr3-todo-list/src/components/Table.jsx
@@ -1,34 +1,63 @@
-const Table = ({ tasks ,updateStatus }) => {
-    
+import { useEffect, useState } from "react"
+const Table = ({ tasks, setTasks, filter }) => {
+    const [filterTasks, setFilterTasks] = useState([]);
+    const completedCount = filterTasks.filter(task => task.isCompleted).length;
+    const pendingCount = filterTasks.length - completedCount;
+
+    const updateStatus = (id) => {
+        let updatedTask = tasks.map((task) => {
+            return task.id === id ? { ...task, isCompleted: true } : task;
+        })
+        setTasks(updatedTask)
+    }
+
+    useEffect(() => {
+        let filterTask = tasks.filter((task) => {
+            return filter === "pending" ? !task.isCompleted : filter == "completed" ? task.isCompleted : true
+        });
+        setFilterTasks(filterTask)
+    }, [tasks, filter])
+
     return (
         <>
-            <div className="relative overflow-x-auto mt-5">
-                <table className="w-full border border-black text-sm text-left rtl:text-right text-gray-500">
-                    <thead className="text-xs text-white uppercase bg-blue-600">
+            <div className="rounded-md mt-5 max-h-64 overflow-y-auto px-2 custom-scroll">
+                <table className="w-full text-sm text-left text-white border-separate border-spacing-y-2">
+                    {/* <thead className="text-xs text-white uppercase bg-rose-500">
                         <tr>
-                            <th scope="col" className="px-6 py-3">
-                                Tasks List
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Status
-                            </th>
+                            <th scope="col" className="px-4 py-3">Tasks List</th>
+                            <th scope="col" className="px-4 py-3">Status</th>
                         </tr>
-                    </thead>
+                    </thead> */}
                     <tbody>
-                        {tasks.map((task) => {
-                            return <tr key={task.id} className="bg-white border-b border-gray-200">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                    {task.taskName}
-                                </th>
-                                {task.isCompleted ? <td className="px-6 py-4">
-                                    Task Completed
-                                </td> : <td className="px-6 py-4" onClick={() => {
-                                    updateStatus(task.id);
-                                }}>
-                                    Pending
-                                </td>}
+                        {filterTasks.map((task) => {
+                            return <tr key={task.id} className="bg-neutral-600 border border-white rounded-md">
+                                <td className="px-4 py-3 text-white whitespace-nowrap font-normal">
+                                    <div className="flex items-center gap-2">
+                                        <input type="checkbox" checked={task.isCompleted}
+                                            onChange={() => updateStatus(task.id)}
+                                            className="accent-blue-600 w-4 h-4" />
+                                        <span className={task.isCompleted ? 'line-through text-gray-400' : ''}>
+                                            {task.taskName}
+                                        </span>
+                                    </div>
+                                </td>
+                                <td className="px-4 py-3">
+                                    {task.isCompleted ? (
+                                        <span className="text-green-400">Completed</span>
+                                    ) : (
+                                        <span
+                                            onClick={() => updateStatus(task.id)}
+                                            className="cursor-pointer text-yellow-300">
+                                            Pending
+                                        </span>
+                                    )}
+                                </td>
                             </tr>
                         })}
+                        <tr className="font-semibold">
+                            <td className="px-2 py-3 text-white">Completed: {completedCount}</td>
+                            <td className="px-2 py-3 text-white">Pending: {pendingCount}</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
--- pr3-todo-list/src/components/TodoList.jsx
+++ pr3-todo-list/src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import Table from "./Table";
 
 const TodoList = () => {
@@ -6,14 +6,6 @@ const TodoList = () => {
     const [tasks, setTasks] = useState([]);
     const ipRef = useRef(null);
     const [filter, setFilter] = useState("all");
-    const [filterTasks, setFilterTasks] = useState([]);
-
-    const updateStatus = (id) => {
-        let updatedTask = tasks.map((task) => {
-            return task.id === id ? { ...task, isCompleted: true } : task;
-        })
-        setTasks(updatedTask)
-    }
 
     const addTask = () => {
         if (text.trim() === "") return;
@@ -29,51 +21,39 @@ const TodoList = () => {
         setText("")
     }
 
-    useEffect(() => {
-        let updatedTask = []
-
-        if (filter == "all") {
-            updatedTask = tasks;
-        } else if (filter == "pending") {
-            updatedTask = tasks.filter((item) => !item.isCompleted)
-            console.log(updatedTask);
-        } else {
-            updatedTask = tasks.filter((item) => item.isCompleted)
-        }
-        setFilterTasks(updatedTask)
-    }, [tasks, filter])
-
     return (
         <>
-            <h1 className="text-center mt-3 text-3xl font-semibold">Todo List</h1>
-            <div className="container mx-auto">
-                <div className="w-6/12 mx-auto">
-                    <form className="flex items-center max-w-lg mx-auto">
-                        <div className="flex mt-4 w-full">
-                            <input ref={ipRef} type="text" onChange={(e) => {
-                                setText(e.target.value);
-                            }} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-8/12 p-2.5" placeholder="Enter Your Tasks" required />
-                            <button type="button" onClick={addTask}
-                                className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                                Add Task
-                            </button>
+            <div className="pt-10 max-w-md mx-auto p-6">
+                <div className="text-white box p-6 rounded-2xl shadow-xl border-2 border-blue-500">
+                    <h1 className="text-2xl font-bold uppercase text-center mb-2">Todo List</h1>
+                    <div className="">
+                        <form className="flex items-center max-w-lg mx-auto">
+                            <div className="flex mt-4 w-full">
+                                <input ref={ipRef} type="text" onChange={(e) => {
+                                    setText(e.target.value);
+                                }} className="rounded-md px-2 text-black focus:outline-none max-w-xs mr-2" placeholder="Enter Your Tasks" required />
+                                <button type="button" onClick={addTask}
+                                    className="bg-teal-500 hover:bg-teal-700 px-4 py-2 rounded-md font-semibold text-xs uppercase">
+                                    Add Task
+                                </button>
+                            </div>
+                        </form>
+                        <div className="flex justify-center">
+                            <div className="inline-flex mt-6 shadow-xs rounded-full overflow-hidden" role="group">
+                                <button onClick={() => { setFilter("all") }} type="button" className="bg-amber-300 hover:bg-amber-500 hover:text-white text-gray-800 font-semibold px-4 py-2 border border-amber-600 text-xs uppercase ">
+                                    All
+                                </button>
+                                <button onClick={() => { setFilter("pending") }} type="button" className="bg-amber-300 hover:bg-amber-600 hover:text-white text-gray-800 font-semibold px-4 py-2 border border-amber-600 text-xs uppercase ">
+                                    Pending
+                                </button>
+                                <button onClick={() => { setFilter("completed") }} type="button" className="bg-amber-300 hover:bg-amber-600 hover:text-white text-gray-800 font-semibold px-4 py-2 border border-amber-600 text-xs uppercase ">
+                                    Completed
+                                </button>
+                            </div>
                         </div>
-                    </form>
-                    <div className="flex justify-center">
-                        <div className="inline-flex rounded-md shadow-xs mt-5" role="group">
-                            <button onClick={() => { setFilter("all") }} type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-                                All
-                            </button>
-                            <button onClick={() => { setFilter("pending") }} type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-                                Pending
-                            </button>
-                            <button onClick={() => { setFilter("completed") }} type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-                                Completed
-                            </button>
-                        </div>
-                    </div>
+                        <Table tasks={tasks} setTasks={setTasks} filter={filter} />
 
-                    <Table tasks={filterTasks} updateStatus={updateStatus} />
+                    </div>
                 </div>
             </div>
         </>
