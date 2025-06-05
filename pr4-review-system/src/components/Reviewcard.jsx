e8364152e3266420f1908927fd884b78e49daef7
shreyaanjan <shreyaanjan02@gmail.com>
2025-06-05 22:05:54 +0530

    errors solved

--- pr4-review-system/src/components/Reviewcard.jsx
+++ pr4-review-system/src/components/Reviewcard.jsx
@@ -54,9 +54,9 @@ const Reviewcard = () => {
             <div className={`flex w-full max-w-5xl gap-5 flex-col md:flex-row 
                 ${submittedData.length === 0 ? "justify-center items-center" : ""}`}>
                 <form ref={formRef} onSubmit={handleSubmit} className="bg-white/60 backdrop-blur-lg shadow-xl rounded-xl p-6 w-full h-full max-w-md">
-                    <h2 className="text-3xl font-bold text-center mb-6">Review Form</h2>
+                    <h2 className="text-2xl font-bold text-center mb-6">Review Form</h2>
                     <div className="mb-5 pb-2 relative">
-                        <label htmlFor="name" className="block mb-0.5 px-1 absolute bg-white left-3 top-[-11px] text-sm font-medium text-gray-700">Name</label>
+                        <label htmlFor="name" className="block mb-2 px-1 absolute bg-white left-3 top-[-11px] text-sm font-medium text-gray-700">Name</label>
                         <input type="text" id="name" name="name" value={input.name} onChange={handleChange} className="w-full px-2.5 py-2.5 text-sm border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 focus:outline-none" placeholder="Enter your name" required />
                         {errors.name && <p className="text-sm text-red-600">{errors.name}</p>}
                     </div>
@@ -67,23 +67,14 @@ const Reviewcard = () => {
                     </div>
                     <div className='flex gap-3 items-center'>
                         <div className="relative mb-5">
-                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                <svg className="w-5 h-5 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                                </svg>
-                            </div>
-                            <label htmlFor="rating" className="block mb-2 px-1 absolute bg-white left-4 top-[-11px] text-sm font-medium text-gray-700">
-                                Date
-                            </label>
+                            <label htmlFor="rating" className="block mb-2 px-1 absolute bg-white left-4 top-[-11px] text-sm font-medium text-gray-700">Date</label>
                             <div className=''>
-                                <input id="date" value={input.date} onChange={handleChange} type="date" className="pl-10 pr-4 py-2.5 text-sm w-48 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 focus:outline-none" placeholder="Select date" />
+                                <input id="date" value={input.date} onChange={handleChange} type="date" className="px-2.5 py-2.5 text-sm w-48 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 focus:outline-none" placeholder="Select date" />
                                 {errors.date && <p className="text-sm text-red-600 dark:text-red-500">{errors.date}</p>}
                             </div>
                         </div>
                         <div className="mb-5 relative">
-                            <label htmlFor="rating" className="block mb-2 px-1 absolute bg-white left-3 top-[-11px] text-sm font-medium text-gray-700">
-                                Rating
-                            </label>
+                            <label htmlFor="rating" className="block mb-2 px-1 absolute bg-white left-3 top-[-11px] text-sm font-medium text-gray-700">Rating</label>
                             <select name="rating" value={input.rating} onChange={handleChange} id="rating" className="w-48 px-2.5 py-2.5 text-sm border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-400 focus:outline-none">
                                 <option value="0">Rate Here</option>
                                 <option value="1">1</option>
--- pr5-bootstrap-landing-page/src/components/About.jsx
+++ pr5-bootstrap-landing-page/src/components/About.jsx
@@ -6,6 +6,10 @@ const About = () => {
                     <div className="col-6">
                         <h5 className="text-uppercase">Story about us</h5>
                         <h2>Create your app page <br /> with expert developer</h2>
+                        <p className="mt-4">
+                            Both rest of know draw fond post as. It agreement defective to excellent. Feebly do engage of narrow. Extensive repulsive belonging depending if promotion be zealously as. Preference inquietude ask now are dispatched led appearance. Small meant in so doubt hopes. 
+                        </p>
+                        <button className="rounded-pill head-btn text-white text-capitalize">read more</button>
                     </div>
                 </div>
             </div>
