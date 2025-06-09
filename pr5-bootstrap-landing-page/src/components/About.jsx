100a7f667beacea61115d4325d8b2c6e61ec5507
shreyaanjan <shreyaanjan02@gmail.com>
2025-06-09 12:22:37 +0530

    buttor hover

--- pr5-bootstrap-landing-page/src/components/About.jsx
+++ pr5-bootstrap-landing-page/src/components/About.jsx
@@ -10,6 +10,30 @@ const About = () => {
                             Both rest of know draw fond post as. It agreement defective to excellent. Feebly do engage of narrow. Extensive repulsive belonging depending if promotion be zealously as. Preference inquietude ask now are dispatched led appearance. Small meant in so doubt hopes. 
                         </p>
                         <button className="rounded-pill head-btn text-white text-capitalize">read more</button>
+                        <div className="bottom-info mt-5 border-top pt-4">
+                            <h3>Why Choose Us</h3>
+                            <ul className="p-0">
+                                <li className="my-3 d-flex gap-3 align-items-center">
+                                    <span>
+                                    <i className="bi bi-check "></i>
+                                    </span>
+                                    <p className="m-0">Elderly is detract tedious assured private</p>
+                                </li>
+                                <li  className="my-3 d-flex gap-3 align-items-center">
+                                    <span>
+                                    <i className="bi bi-check "></i>
+
+                                    </span>
+                                    <p className="m-0">Do travelling companions contrasted</p>
+                                </li>
+                                <li  className="my-3 d-flex gap-3 align-items-center">
+                                    <span>
+                                    <i className="bi bi-check "></i>
+                                    </span>
+                                    <p className="m-0">Mistress strongly remember up</p>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
