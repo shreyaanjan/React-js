f3583a85810ce5e64472a4f78d788786e4d0e277
shreyaanjan <shreyaanjan02@gmail.com>
2025-06-12 13:51:35 +0530

    minor change

--- pr5-bootstrap-landing-page/src/components/Pricing.jsx
+++ pr5-bootstrap-landing-page/src/components/Pricing.jsx
@@ -2,6 +2,45 @@ import PricingCard from "./PricingCard";
 import Title from "./Title"
 
 const Pricing = () => {
+    let plan = [
+        {
+            iconArr: [
+                {catagory:"trial version", price: "00.00"},
+                {label: "Demo File",validation: true},
+                {label: "Update",validation: false},
+                {label: "File Compressed",validation: true},
+                {label: "Commercial use",validation: true},
+                {label: "Support",validation: false},
+                {label: "2 Database",validation: false},
+                {label: "Documentation",validation: true},
+            ]
+        },
+        {
+            iconArr: [
+                {catagory:"Regular", price: "29.00"},
+                {label: "Demo File",validation: true},
+                {label: "Update",validation: true},
+                {label: "File Compressed",validation: true},
+                {label: "Commercial use",validation: false},
+                {label: "Support",validation: true},
+                {label: "5 Database",validation: false},
+                {label: "Documentation",validation: true},
+            ]
+        },
+        {
+            iconArr: [
+                {catagory:"Extended", price: "59.00"},
+                {label: "Demo File",validation: true},
+                {label: "Update",validation: true},
+                {label: "File Compressed",validation: false},
+                {label: "Commercial use",validation: true},
+                {label: "Support",validation: false},
+                {label: "8 Database",validation: true},
+                {label: "Documentation",validation: true},
+            ]
+        },
+       
+    ]
     let title = "Our Packages";
     let desc = "Learning day desirous informed expenses material returned six the.She enabled invited exposed him another.Reasonably conviction solicitude me mr at discretion"
     return (
@@ -11,7 +50,9 @@ const Pricing = () => {
             </div>
             <div className="container">
                 <div className="row">
-                    <PricingCard />
+                    {plan.map((obj)=>{
+                        return <PricingCard iconArr={obj.iconArr} />
+                    })}
                 </div>
             </div>
         </section>
--- pr5-bootstrap-landing-page/src/components/PricingCard.jsx
+++ pr5-bootstrap-landing-page/src/components/PricingCard.jsx
@@ -1,16 +1,24 @@
-const PricingCard = () => {
+import { TiTickOutline } from "react-icons/ti";
+import { IoClose } from "react-icons/io5";
+
+const PricingCard = ({ iconArr }) => {
+    const {catagory, price} = iconArr[0];
+    const features = iconArr.slice(1);
     return (
         <div className="col-4">
             <div className="pricing-item mt-5 rounded-4">
-                <ul className="p-0">
-                    <li className="pricing-header text-white">
-                        <h4>Trial Version</h4>
-                        <h2>
-                            <sup>$</sup>00.00 <sub>/Mo</sub>
-                        </h2>
-                    </li>
-                    <li></li>
-                </ul>
+               
+                     <ul className="p-0">
+                        <li className="pricing-header text-white">
+                            <h4>{catagory}</h4>
+                            <h2>
+                                <sup>$</sup>{price}<sub>/Mo</sub>
+                            </h2>
+                        </li>
+                        {features.map((item)=>{
+                            return<li>{item.validation ? <TiTickOutline />: <IoClose />}{item.label}</li>
+                        })}  
+                    </ul>
             </div>
         </div>
     )
